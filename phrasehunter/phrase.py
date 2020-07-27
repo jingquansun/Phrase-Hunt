34baaf4f17ea4b97577e6fa947e3d26364f035c3
jingquansun <jingquan.sun5@gmail.com>
2020-07-27 17:05:23 -0500

    Create phrase.py

---
+++ phrasehunter/phrase.py
@@ -0,0 +1,31 @@
+from phrasehunter.character import Character
+
+class Phrase:
+  def __init__(self, phrase):
+    letters = list(phrase)
+    self.characters = [Character(letter) for letter in letters]
+    
+  def guess_complete(self):
+    guessed_phrase = []
+    for letter in self.characters:
+      guessed_phrase.append(letter.show())
+    return "_" not in guessed_phrase
+  
+  def entire_phrase(self):
+    for letter in self.characters:
+      print(letter.show(), end="")
+  
+  def correct_guess(self, guess):
+    count = 0
+    for letter in self.characters:
+      if letter.equal(guess):
+        count += 1
+        letter.set_guess()
+    return count >= 1
+  
+  def show_everything(self):
+    for letter in self.characters:
+      print(letter.show_everything(), end="")
+    
+            
+        

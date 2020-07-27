db53d65be99da458f9e66f63560a53446c79a193
jingquansun <jingquan.sun5@gmail.com>
2020-07-27 17:05:11 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from phrasehunter.game import Game
+
+phrase = ["Piece of cake",
+          "Burst your bubble",
+          "Cut to the chase",
+          "My cup of tea",
+          "Out of the park",
+          "Hands down the best"] 
+
+
+def main():
+  game = Game(phrase)
+  game.start_game()
+
+
+
+
+
+if __name__ == "__main__":
+  main()
